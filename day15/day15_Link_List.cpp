18f01c03a31958c5fd62f725ad983be4c978fd8b
LAPTOP-891TQN02\iversonshao <whistle327@gmail.com>
2024-01-04 03:19:14 +0800

    v1.0

---
+++ day15/day15_Link_List.cpp
@@ -0,0 +1,50 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node *next;
+    Node(int d){
+        data=d;
+        next=NULL;
+    }
+};
+
+Node* insert(Node* head, int data){
+    if(head == NULL){
+        head = new Node(data);
+    }
+    else{
+        Node *last = head;
+        while(last->next != NULL){
+            last = last->next;
+        }
+        last->next = new Node(data);
+    }
+    return head;
+}
+
+void display(Node* head){
+    Node *start = head;
+    while(start){
+        cout << start->data << " ";
+        start = start->next;
+    }
+}
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    Node* head = NULL;
+    int T, data;
+    cin >> T;
+    while(T-- > 0){
+        cin >> data;
+        head = insert(head, data);
+    }
+    display(head);
+    return 0;
+}
