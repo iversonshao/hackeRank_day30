2aefb436dc0d81d20a2cf9ea3b517e93292c6dc5
LAPTOP-891TQN02\iversonshao <whistle327@gmail.com>
2024-01-10 04:29:30 +0800

    v1.0

---
+++ day21/day21_Generics.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+template <class T> 
+void printArray(vector<T> a){
+    for(int i = 0; i < a.size(); ++i){
+        cout << a[i] << endl;
+    }     
+}
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    cin >> n;
+
+    vector<int> int_vector(n);
+    for(int i = 0; i < n; ++i){
+        int value;
+        cin >> value;
+        int_vector[i] = value;
+    }
+    
+    cin >> n;
+    vector<string> string_vector(n);
+    for(int i = 0; i < n; ++i){
+        string value;
+        cin >> value;
+        string_vector[i] = value;
+    }
+    printArray<int>(int_vector);
+    printArray<string>(string_vector);
+    return 0;
+}
